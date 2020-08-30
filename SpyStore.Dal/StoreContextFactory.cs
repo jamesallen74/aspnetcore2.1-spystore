0eb20e56810dc7f8b68b7d3d610f3797bcfdc70a
James Allen <jamesallen74@gmail.com>
2020-08-29 20:23:50 -0400

    Init commit. Chapters 1, 2, and first half of 3

---
+++ SpyStore.Dal/StoreContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System.Diagnostics;
+
+namespace SpyStore.Dal
+{
+    public class StoreContextFactory : IDesignTimeDbContextFactory<StoreContext> 
+    { 
+        public StoreContext CreateDbContext(string[] args) 
+        { 
+            var optionsBuilder = new DbContextOptionsBuilder<StoreContext>(); 
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SpyStore21;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; 
+            optionsBuilder.UseSqlServer(connectionString, options => options.EnableRetryOnFailure()); 
+            optionsBuilder.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning)); 
+            Debug.WriteLine(connectionString); 
+            return new StoreContext(optionsBuilder.Options); 
+        } 
+    }
+}
